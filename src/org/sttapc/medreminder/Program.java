158b9e93f5bdfd85f1829fd5d36c148c76818d14
Shiphter <stephantl@gmail.com>
2014-03-05 09:38:29 +0100

    Adding reminder sound, not working

--- src/org/sttapc/medreminder/Program.java
+++ src/org/sttapc/medreminder/Program.java
@@ -1,68 +1,14 @@
 package org.sttapc.medreminder;
 
-import java.io.IOException;
-
-import org.sttapc.medreminder.util.Initializer;
-
-import com.phidgets.InterfaceKitPhidget;
-import com.phidgets.PhidgetException;
-import com.phidgets.event.AttachEvent;
-import com.phidgets.event.AttachListener;
-import com.phidgets.event.InputChangeEvent;
-import com.phidgets.event.InputChangeListener;
-import com.phidgets.event.SensorChangeEvent;
-import com.phidgets.event.SensorChangeListener;
+import org.sttapc.medreminder.util.Reminder;
 
 public class Program {
 
-	public static InterfaceKitPhidget interfaceKitPhidget = null;
-
-	public static AttachListener attachListener = new AttachListener() {
-		@Override
-		public void attached(AttachEvent arg0) {
-			try {
-				System.out.println("Interfacekit Attached!");
-				System.out.println("Device name: "
-						+ interfaceKitPhidget.getDeviceName());
-				interfaceKitPhidget
-						.addSensorChangeListener(sensorChangeListener);
-
-				interfaceKitPhidget.setOutputState(1, false);
-				interfaceKitPhidget.setOutputState(0, false);
-				interfaceKitPhidget.addInputChangeListener(inputChangeListener);
-			} catch (PhidgetException e) {
-				HandleException(e);
-			}
-		}
-	};
-
-	public static SensorChangeListener sensorChangeListener = new SensorChangeListener() {
-
-		@Override
-		public void sensorChanged(SensorChangeEvent arg0) {
-			System.out.println("SensorChanged");
-			System.out.println("Sensor value : " + arg0.getValue());
-
-		}
-	};
-
-	public static InputChangeListener inputChangeListener = new InputChangeListener() {
-
-		@Override
-		public void inputChanged(InputChangeEvent arg0) {
-			System.out.println("InputChanged");
-
-			System.out.println("Input value : " + arg0.getState()
-					+ " on index: " + arg0.getIndex());
-		}
-	};
-
 	public static void main(String[] args) {
-		Initializer initializer = new Initializer();
+		Reminder reminder = new Reminder();
 		try {
-			initializer.setupConfigurator("resources/configurator.json");
-			initializer.persistConfigurator("resources/configurator.json");
-		} catch (IOException e) {
+			reminder.NearbyActiveReminder();
+		} catch (Exception e) {
 			HandleException(e);
 		}
 	}
--- src/org/sttapc/medreminder/util/Reminder.java
+++ src/org/sttapc/medreminder/util/Reminder.java
@@ -2,12 +2,23 @@ package org.sttapc.medreminder.util;
 
 public class Reminder {
 
+	private MakeSound makeSound;
+	private String nearbyActiveSoundPath = "resources/nearbyActiveSound.mp3";
+	private String nearbyWarningSoundPath = "resources/nearbyWarningSound.mp3";
+
+	public Reminder() {
+		makeSound = new MakeSound();
+	}
+
 	public void NearbyActiveReminder() {
 		System.out.println("REMINDER: Patient Nearby - Take medication");
+		makeSound.playSound(nearbyActiveSoundPath);
 	}
 
 	public void NearbyWarningReminder() {
 		System.out
 				.println("REMINDER: Patient Nearby - Warning, take medication now!");
+		makeSound.playSound(nearbyWarningSoundPath);
 	}
+
 }
