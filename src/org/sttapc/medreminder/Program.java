9d7ee56be4efebcedc24381b7869f33cca0c191a
Shiphter <stephantl@gmail.com>
2014-03-06 14:07:01 +0100

    Sound reminder now works.

--- src/org/sttapc/medreminder/Program.java
+++ src/org/sttapc/medreminder/Program.java
@@ -1,50 +1,49 @@
 package org.sttapc.medreminder;
 
-import org.sttapc.medreminder.util.MakeSound;
 
 public class Program {
 
-	public static void main(String[] args) {
-
-		MakeSound makeSound = new MakeSound();
-		makeSound.playSound("resources/nearbyActiveSound.wav");
-		// Configurator configurator = new Configurator();
-		// configurator.setSchedule(new Schedule(new Date()));
-		// configurator.setLogning(new Logning());
-		// Initializer initializer = new Initializer();
-		// initializer.setConfigurator(configurator);
-		// initializer.persistConfigurator("resources/configurator1.json");
-
-		//
-		// try {
-		// final InterfaceKitPhidget interfaceKitPhidget = new
-		// InterfaceKitPhidget();
-		// interfaceKitPhidget.addAttachListener(new AttachListener() {
-		//
-		// @Override
-		// public void attached(AttachEvent arg0) {
-		// try {
-		// System.out.println("InterfaceKit Attached");
-		// Sound sound = new Sound(interfaceKitPhidget, 0);
-		// sound.PlaySound(440);
-		// } catch (Exception e) {
-		// HandleException(e);
-		// }
-		//
-		// }
-		// });
-		// interfaceKitPhidget.openAny();
-		// while (true) {
-		//
-		// }
-		// } catch (Exception e) {
-		// HandleException(e);
-		// }
-	}
-
-	public static void HandleException(Exception e) {
-		System.out.println("An error occurred: " + e.getMessage());
-		System.out.println("Stacktrace: ");
-		e.printStackTrace();
-	}
+	// public static void main(String[] args) {
+	//
+	// MakeSound makeSound = new MakeSound();
+	// makeSound.playSound("resources/nearbyActiveSound.wav");
+	// // Configurator configurator = new Configurator();
+	// // configurator.setSchedule(new Schedule(new Date()));
+	// // configurator.setLogning(new Logning());
+	// // Initializer initializer = new Initializer();
+	// // initializer.setConfigurator(configurator);
+	// // initializer.persistConfigurator("resources/configurator1.json");
+	//
+	// //
+	// // try {
+	// // final InterfaceKitPhidget interfaceKitPhidget = new
+	// // InterfaceKitPhidget();
+	// // interfaceKitPhidget.addAttachListener(new AttachListener() {
+	// //
+	// // @Override
+	// // public void attached(AttachEvent arg0) {
+	// // try {
+	// // System.out.println("InterfaceKit Attached");
+	// // Sound sound = new Sound(interfaceKitPhidget, 0);
+	// // sound.PlaySound(440);
+	// // } catch (Exception e) {
+	// // HandleException(e);
+	// // }
+	// //
+	// // }
+	// // });
+	// // interfaceKitPhidget.openAny();
+	// // while (true) {
+	// //
+	// // }
+	// // } catch (Exception e) {
+	// // HandleException(e);
+	// // }
+	// }
+	//
+	// public static void HandleException(Exception e) {
+	// System.out.println("An error occurred: " + e.getMessage());
+	// System.out.println("Stacktrace: ");
+	// e.printStackTrace();
+	// }
 }
--- src/org/sttapc/medreminder/util/Reminder.java
+++ src/org/sttapc/medreminder/util/Reminder.java
@@ -2,24 +2,23 @@ package org.sttapc.medreminder.util;
 
 public class Reminder {
 
-	// private MakeSound makeSound;
-	// private String nearbyActiveSoundPath = "resources/nearbyActiveSound.mp3";
-	// private String nearbyWarningSoundPath =
-	// "resources/nearbyWarningSound.mp3";
+	private MakeSound makeSound;
+	private String nearbyActiveSoundPath = "resources/nearbyActiveSound.mp3";
+	private String nearbyWarningSoundPath = "resources/nearbyWarningSound.mp3";
 
 	public Reminder() {
-		// makeSound = new MakeSound();
+		makeSound = new MakeSound();
 	}
 
 	public void NearbyActiveReminder() {
 		System.out.println("REMINDER: Patient Nearby - Take medication");
-		// makeSound.playSound(nearbyActiveSoundPath);
+		makeSound.playSound(nearbyActiveSoundPath);
 	}
 
 	public void NearbyWarningReminder() {
 		System.out
 				.println("REMINDER: Patient Nearby - Warning, take medication now!");
-		// makeSound.playSound(nearbyWarningSoundPath);
+		makeSound.playSound(nearbyWarningSoundPath);
 	}
 
 }
